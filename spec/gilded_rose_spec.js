15d519fc0a7b1eb539a88360e4c7ce02b05b76a1
Seungjin Kim <skim@proove.com>
2021-03-15 21:12:56 -0700

    UPDATE: first test to pass

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,13 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+var { Shop, Item } = require("../src/gilded_rose.js");
+describe("Gilded Rose", function () {
+  it("should lower both sellIn and quality after one day", function () {
+    const startCount = 3;
+    const gildedRose = new Shop([new Item("foo", startCount, startCount)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
-  });
+    const { name, sellIn, quality } = items[0];
 
+    expect(name).toEqual("foo");
+    expect(sellIn).toEqual(startCount - 1);
+    expect(quality).toEqual(startCount - 1);
+  });
 });
