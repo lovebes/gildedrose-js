9a4dfce66210ee6aa8087c1ba9ba95b4ce0e8efc
Seungjin Kim <skim@proove.com>
2021-03-15 22:02:11 -0700

    UPDATE: constants usage

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -6,27 +6,26 @@ class Item {
   }
 }
 
+const AGED_BRIE = "Aged Brie";
+const SULFRAS = "Sulfuras, Hand of Ragnaros";
+const BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+
 class Shop {
   constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
+      if (this.items[i].name != AGED_BRIE && this.items[i].name != BACKSTAGE) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (this.items[i].name != SULFRAS) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (this.items[i].name == BACKSTAGE) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -40,16 +39,14 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+      if (this.items[i].name != SULFRAS) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+        if (this.items[i].name != AGED_BRIE) {
+          if (this.items[i].name != BACKSTAGE) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (this.items[i].name != SULFRAS) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
