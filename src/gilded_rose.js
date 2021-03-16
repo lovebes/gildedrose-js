e2679ddd6594d0857b6983e7e1aae4b1cb143316
Seungjin Kim <skim@proove.com>
2021-03-15 22:11:20 -0700

    UPDATE: refactored the normal item case

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,56 +10,80 @@ const AGED_BRIE = "Aged Brie";
 const SULFRAS = "Sulfuras, Hand of Ragnaros";
 const BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
 
+const handleNormalItem = ({ sellIn, quality, name }) => {
+  const newSellIn = sellIn - 1;
+  let newQuality = quality;
+  if (newSellIn < 0) {
+    newQuality -= 2;
+  } else {
+    newQuality -= 1;
+  }
+  newQuality = Math.max(0, newQuality);
+  return {
+    name,
+    sellIn: newSellIn,
+    quality: newQuality,
+  };
+};
+
+const updateItem = (item, newItem) => {
+  item.sellIn = newItem.sellIn;
+  item.quality = newItem.quality;
+};
+
 class Shop {
   constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != AGED_BRIE && this.items[i].name != BACKSTAGE) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != SULFRAS) {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == BACKSTAGE) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != SULFRAS) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != AGED_BRIE) {
-          if (this.items[i].name != BACKSTAGE) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != SULFRAS) {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+      const item = this.items[i];
+
+      updateItem(item, handleNormalItem(item));
+      // if (this.items[i].name != AGED_BRIE && this.items[i].name != BACKSTAGE) {
+      //   if (this.items[i].quality > 0) {
+      //     if (this.items[i].name != SULFRAS) {
+      //       this.items[i].quality = this.items[i].quality - 1;
+      //     }
+      //   }
+      // } else {
+      //   if (this.items[i].quality < 50) {
+      //     this.items[i].quality = this.items[i].quality + 1;
+      //     if (this.items[i].name == BACKSTAGE) {
+      //       if (this.items[i].sellIn < 11) {
+      //         if (this.items[i].quality < 50) {
+      //           this.items[i].quality = this.items[i].quality + 1;
+      //         }
+      //       }
+      //       if (this.items[i].sellIn < 6) {
+      //         if (this.items[i].quality < 50) {
+      //           this.items[i].quality = this.items[i].quality + 1;
+      //         }
+      //       }
+      //     }
+      //   }
+      // }
+      // if (this.items[i].name != SULFRAS) {
+      //   this.items[i].sellIn = this.items[i].sellIn - 1;
+      // }
+      // if (this.items[i].sellIn < 0) {
+      //   if (this.items[i].name != AGED_BRIE) {
+      //     if (this.items[i].name != BACKSTAGE) {
+      //       if (this.items[i].quality > 0) {
+      //         if (this.items[i].name != SULFRAS) {
+      //           this.items[i].quality = this.items[i].quality - 1;
+      //         }
+      //       }
+      //     } else {
+      //       this.items[i].quality =
+      //         this.items[i].quality - this.items[i].quality;
+      //     }
+      //   } else {
+      //     if (this.items[i].quality < 50) {
+      //       this.items[i].quality = this.items[i].quality + 1;
+      //     }
+      //   }
+      // }
     }
 
     return this.items;
