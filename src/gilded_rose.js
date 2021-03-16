17e6b729652c0fcebba649a029019896059f46ed
Seungjin Kim <skim@proove.com>
2021-03-15 22:32:44 -0700

    REMOVE: commented out original code

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -125,51 +125,6 @@ class Shop {
           updateItem(item, handleNormalItem(item));
           break;
       }
-
-      // if (this.items[i].name != AGED_BRIE && this.items[i].name != BACKSTAGE) {
-      //   if (this.items[i].quality > 0) {
-      //     if (this.items[i].name != SULFRAS) {
-      //       this.items[i].quality = this.items[i].quality - 1;
-      //     }
-      //   }
-      // } else {
-      //   if (this.items[i].quality < 50) {
-      //     this.items[i].quality = this.items[i].quality + 1;
-      //     if (this.items[i].name == BACKSTAGE) {
-      //       if (this.items[i].sellIn < 11) {
-      //         if (this.items[i].quality < 50) {
-      //           this.items[i].quality = this.items[i].quality + 1;
-      //         }
-      //       }
-      //       if (this.items[i].sellIn < 6) {
-      //         if (this.items[i].quality < 50) {
-      //           this.items[i].quality = this.items[i].quality + 1;
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
-      // if (this.items[i].name != SULFRAS) {
-      //   this.items[i].sellIn = this.items[i].sellIn - 1;
-      // }
-      // if (this.items[i].sellIn < 0) {
-      //   if (this.items[i].name != AGED_BRIE) {
-      //     if (this.items[i].name != BACKSTAGE) {
-      //       if (this.items[i].quality > 0) {
-      //         if (this.items[i].name != SULFRAS) {
-      //           this.items[i].quality = this.items[i].quality - 1;
-      //         }
-      //       }
-      //     } else {
-      //       this.items[i].quality =
-      //         this.items[i].quality - this.items[i].quality;
-      //     }
-      //   } else {
-      //     if (this.items[i].quality < 50) {
-      //       this.items[i].quality = this.items[i].quality + 1;
-      //     }
-      //   }
-      // }
     }
 
     return this.items;
